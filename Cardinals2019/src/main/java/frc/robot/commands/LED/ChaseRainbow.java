eecd993751663876dbcc297072599097560079aa
Bryce MacLean <bryce@Bryces-MacBook-Air.local>
2019-02-09 14:34:24 -0500

    Command LEDs

---
+++ Cardinals2019/src/main/java/frc/robot/commands/LED/ChaseRainbow.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.LED;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+/**
+ * Add your docs here.
+ */
+public class ChaseRainbow extends InstantCommand {
+  /**
+   * Add your docs here.
+   */
+  public ChaseRainbow() {
+    super();
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.LED);
+  }
+
+  // Called once when the command executes
+  @Override
+  protected void initialize() {
+    Robot.LED.setLEDs(13);
+    }
+  
+  }
