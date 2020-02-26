3deb913e58f82c398e11cf683bac1809db142af6
Julia F <jjfino02@gmail.com>
2020-02-25 20:45:50 -0500

    color wheel

--- src/main/java/frc/robot/subsystems/ColorWheel.java
+++ src/main/java/frc/robot/subsystems/ColorWheel.java
@@ -9,7 +9,11 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -20,8 +24,8 @@ import frc.robot.RobotMap;
 
 /**
  * This is a subsystem class. A subsystem interacts with the hardware components
- * on the robot. This subsystem deals with the color wheel and contains methods to
- * automatically perform both rotation and position control
+ * on the robot. This subsystem deals with the color wheel and contains methods
+ * to automatically perform both rotation and position control
  */
 
 public class ColorWheel extends Subsystem implements ISubsystem {
@@ -32,12 +36,25 @@ public class ColorWheel extends Subsystem implements ISubsystem {
         return instance;
     }
 
+    // Store the possible color wheel values in a color matcher
+    private final ColorMatch m_colorMatcher = new ColorMatch();
+
+    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+
     public ColorWheel() {
-        cWheelMotor = new TalonSRX(RobotMap.WHEEL);
+        cWheelMotor = new CANSparkMax(RobotMap.WHEEL, MotorType.kBrushless);
         cSensor = new ColorSensorV3(I2C.Port.kOnboard);
+
+        m_colorMatcher.addColorMatch(kBlueTarget);
+        m_colorMatcher.addColorMatch(kGreenTarget);
+        m_colorMatcher.addColorMatch(kRedTarget);
+        m_colorMatcher.addColorMatch(kYellowTarget);
     }
 
-    private TalonSRX cWheelMotor;
+    private CANSparkMax cWheelMotor;
     private ColorSensorV3 cSensor;
 
     /**
@@ -46,13 +63,13 @@ public class ColorWheel extends Subsystem implements ISubsystem {
      * @param speed (0-1)
      */
     public void rotationControl(double speed) {
-        Color initial = cSensor.getColor();
+        ColorMatchResult initial = m_colorMatcher.matchClosestColor(cSensor.getColor());
         int count = 0;
 
         // the color sensor will see the same color 8 times during one rotation
         while (count < 8) {
-            cWheelMotor.set(ControlMode.PercentOutput, speed);
-            if (cSensor.getColor().equals(initial)) {
+            cWheelMotor.set(speed);
+            if (m_colorMatcher.matchClosestColor(cSensor.getColor()).equals(initial)) {
                 count++;
                 outputSmartdashboard();
             }
@@ -63,26 +80,27 @@ public class ColorWheel extends Subsystem implements ISubsystem {
 
     /**
      * Spins the color wheel to the given color, Blinks LEDs green when complete
+     * 
      * @param speed (0-1)
      */
     public void positionControl(double speed) {
-        String target = Robot.ds.getGameSpecificMessage();
+        String target = "";
 
         if (target.length() > 0) {
             switch (target.charAt(0)) {
             case 'B':
-                // Blue case code
+                goToColor(kBlueTarget, speed);
                 break;
             case 'G':
-                // Green case code
+                goToColor(kGreenTarget, speed);
                 break;
             case 'R':
-                // Red case code
+                goToColor(kRedTarget, speed);
                 break;
             case 'Y':
-                // Yellow case code
+                goToColor(kYellowTarget, speed);
                 break;
-            default:                
+            default:
                 break;
             }
         } else {
@@ -92,13 +110,13 @@ public class ColorWheel extends Subsystem implements ISubsystem {
 
     /**
      * Go to a specified color on the color wheel
+     * 
      * @param c
      * @param speed
      */
     private void goToColor(Color c, double speed) {
-        while(cSensor.getColor()!=c)
-        {
-            cWheelMotor.set(ControlMode.PercentOutput, speed);
+        while (m_colorMatcher.matchClosestColor(cSensor.getColor()).color != c) {
+            cWheelMotor.set(speed);
         }
         stopColorWheel();
         Robot.led.setBlink(96, 200);
@@ -108,12 +126,33 @@ public class ColorWheel extends Subsystem implements ISubsystem {
      * Stop the color wheel motor
      */
     public void stopColorWheel() {
-        cWheelMotor.set(ControlMode.PercentOutput, 0);
+        cWheelMotor.set(0);
+    }
+
+    private String colorString = "";
+
+    private void getCurrentColor() {
+
+        ColorMatchResult match = m_colorMatcher.matchClosestColor(cSensor.getColor());
+
+        if (match.color == kBlueTarget) {
+            colorString = "Blue";
+        } else if (match.color == kRedTarget) {
+            colorString = "Red";
+        } else if (match.color == kGreenTarget) {
+            colorString = "Green";
+        } else if (match.color == kYellowTarget) {
+            colorString = "Yellow";
+        } else {
+            colorString = "Unknown";
+        }
+
     }
 
     @Override
     public void outputSmartdashboard() {
-        SmartDashboard.putString("Color Sensor", cSensor.getColor().toString());
+        getCurrentColor();
+        SmartDashboard.putString("Color Sensor", colorString);
     }
 
     @Override
